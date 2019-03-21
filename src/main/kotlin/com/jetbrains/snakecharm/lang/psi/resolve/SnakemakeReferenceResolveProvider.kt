411ab51f5d66a96af104ea8356b2fede0d2325f8
sharkovadarya <sharkovadarya@gmail.com>
2019-03-21 22:10:45 +0300

    Improved code style, fixed 'expand' FQN.

--- src/main/kotlin/com/jetbrains/snakecharm/lang/psi/resolve/SnakemakeReferenceResolveProvider.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/psi/resolve/SnakemakeReferenceResolveProvider.kt
@@ -1,26 +1,24 @@
 package com.jetbrains.snakecharm.lang.psi.resolve
 
 import com.intellij.psi.util.QualifiedName
-import com.jetbrains.python.psi.PyPsiFacade
-import com.jetbrains.python.psi.PyQualifiedExpression
+import com.jetbrains.python.psi.*
 import com.jetbrains.python.psi.resolve.PyReferenceResolveProvider
 import com.jetbrains.python.psi.resolve.RatedResolveResult
 import com.jetbrains.python.psi.types.TypeEvalContext
 
 class SnakemakeReferenceResolveProvider : PyReferenceResolveProvider {
-    private val namesToFQN = hashMapOf("expand" to "expand") // TODO what is the qualified name of 'expand'?
+    private val namesToFQN = hashMapOf("expand" to "snakemake.io")
 
-    override fun resolveName(element: PyQualifiedExpression, context: TypeEvalContext): MutableList<RatedResolveResult> {
+    override fun resolveName(element: PyQualifiedExpression, context: TypeEvalContext): List<RatedResolveResult> {
         val facade = PyPsiFacade.getInstance(element.project)
-        val fullyQualifiedName = namesToFQN[element.name] ?: return mutableListOf()
+
+        val fullyQualifiedName = namesToFQN[element.name] ?: return emptyList()
         val qName = QualifiedName.fromDottedString(fullyQualifiedName)
 
-        val resolveContext = facade.createResolveContextFromFoothold(element)
-        val resolvedModules = facade.resolveQualifiedName(qName, resolveContext)
+        val resolveContext = facade.createResolveContextFromFoothold(element).copyWithMembers()
 
-        val result = mutableListOf<RatedResolveResult>()
-        result.addAll(resolvedModules.map { RatedResolveResult(RatedResolveResult.RATE_NORMAL, it) })
-
-        return result
+        return facade.resolveQualifiedName(qName, resolveContext)
+                .filterIsInstance<PyFile>()
+                .firstOrNull()?.multiResolveName(element.name!!) ?: emptyList()
     }
 }
