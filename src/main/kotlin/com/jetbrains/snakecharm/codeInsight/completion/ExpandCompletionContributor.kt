4c5bb31a5efae4566bc8c4b91bacab787552cebe
sharkovadarya <sharkovadarya@gmail.com>
2019-04-26 10:54:32 +0300

    Fixed 'expand' completion.

--- src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/ExpandCompletionContributor.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/ExpandCompletionContributor.kt
@@ -1,21 +1,27 @@
 package com.jetbrains.snakecharm.codeInsight.completion
 
 import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.lang.Language
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
 import com.intellij.util.ProcessingContext
+import com.jetbrains.python.codeInsight.completion.addFunctionToResult
+import com.jetbrains.python.codeInsight.completion.createLookupElementBuilder
+import com.jetbrains.python.codeInsight.completion.getFile
+import com.jetbrains.python.psi.PyArgumentList
 import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.PyFile
 import com.jetbrains.python.psi.PyReferenceExpression
 import com.jetbrains.snakecharm.lang.SnakemakeLanguageDialect
 
 class ExpandCompletionContributor : CompletionContributor() {
     companion object {
         private val EXPAND_CAPTURE = PlatformPatterns
-                .psiElement(PsiIdentifier::class.java)
-                .withLanguage(SnakemakeLanguageDialect)
+                .psiElement()
                 .withParent(PyReferenceExpression::class.java)
-                .withParent(PyCallExpression::class.java)
 
         class SnakemakeExpandCompletionProvider(
                 private val keywords: Array<String> = arrayOf("expand")
@@ -24,7 +30,17 @@ class ExpandCompletionContributor : CompletionContributor() {
             override fun addCompletions(parameters: CompletionParameters,
                                         context: ProcessingContext,
                                         result: CompletionResultSet) {
-                result.addAllElements(keywords.map { LookupElementBuilder.create(it) })
+                if (parameters.originalFile.language == Language.findLanguageByID("Python")) {
+                    return
+                }
+
+                result.addAllElements(keywords.map {
+                    LookupElementBuilder
+                            .create(it)
+                            .withInsertHandler(ParenthesesInsertHandler.WITH_PARAMETERS)
+                            .withTailText("(args, wildcards)")
+                            .withTypeText("snakemake.io")
+                })
             }
         }
     }
