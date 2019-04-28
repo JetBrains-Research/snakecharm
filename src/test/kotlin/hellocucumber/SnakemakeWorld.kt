47cd7b768194a7541a645bd57667589cd9703de3
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2019-04-28 16:48:45 +0300

    Playground: try to use cucumber for tests on resolve / completion

---
+++ src/test/kotlin/hellocucumber/SnakemakeWorld.kt
@@ -0,0 +1,11 @@
+package hellocucumber
+
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+
+/**
+ * @author Roman.Chernyatchik
+ * @date 2019-04-28
+ */
+object SnakemakeWorld {
+    lateinit var myFixture: CodeInsightTestFixture
+}
