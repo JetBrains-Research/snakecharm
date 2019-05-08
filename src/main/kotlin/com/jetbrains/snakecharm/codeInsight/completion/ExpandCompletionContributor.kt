40e68cb8a7d6a8f14daa746cff0dfae081ec7641
sharkovadarya <sharkovadarya@gmail.com>
2019-05-08 17:04:58 +0300

    Refactored ExpandCompletionContributor.

--- src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/ExpandCompletionContributor.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/ExpandCompletionContributor.kt
@@ -4,6 +4,7 @@ import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.patterns.PatternCondition
 import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.patterns.PlatformPatterns.psiFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.QualifiedName
@@ -22,6 +23,7 @@ class ExpandCompletionContributor : CompletionContributor() {
 
         private val EXPAND_CAPTURE = psiElement()
                 .inside(REFERENCE)
+                .inFile(psiFile().withLanguage(SnakemakeLanguageDialect))
                 .with(object : PatternCondition<PsiElement>("isFirstChild") {
                     override fun accepts(element: PsiElement, context: ProcessingContext): Boolean {
                         return element.parent.node.firstChildNode == element.node
@@ -31,10 +33,12 @@ class ExpandCompletionContributor : CompletionContributor() {
 
         class SnakemakeExpandCompletionProvider : CompletionProvider<CompletionParameters>() {
 
-            private fun resolveToFunction(file: PsiFile,
-                                          element: PsiElement,
-                                          functionName: String,
-                                          dottedString: String): PyFunction? {
+            private fun resolveToFunction(
+                    file: PsiFile,
+                    element: PsiElement,
+                    functionName: String,
+                    dottedString: String
+            ): PyFunction? {
                 val facade = PyPsiFacade.getInstance(file.project)
                 val resolveContext = facade.createResolveContextFromFoothold(element).copyWithMembers()
                 val qualifiedName = QualifiedName.fromDottedString(dottedString)
@@ -48,16 +52,13 @@ class ExpandCompletionContributor : CompletionContributor() {
                 return null
             }
 
-            override fun addCompletions(parameters: CompletionParameters,
-                                        context: ProcessingContext,
-                                        result: CompletionResultSet) {
-                if (parameters.originalFile.language != SnakemakeLanguageDialect) {
-                    return
-                }
-
+            override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+            ) {
                 val ret = mutableListOf<LookupElement>()
-                val ref = parameters.originalFile.findReferenceAt(parameters.offset - 1) ?: return
-                val element = ref.element
+                val element = parameters.position
 
                 val scopeOwner = resolveToFunction(parameters.originalFile,
                         element, "expand", "snakemake.io")
@@ -65,7 +66,8 @@ class ExpandCompletionContributor : CompletionContributor() {
                 if (scopeOwner != null) {
                     val processor = CompletionVariantsProcessor(element)
                     PyResolveUtil.scopeCrawlUp(processor, scopeOwner, null, null)
-                    ret.addAll(processor.resultList)
+                    // the result list also contains a lot of other snakemake.io functions, not all of which we need
+                    ret.addAll(processor.resultList.filter { it.lookupString.contains("expand") })
                 }
                 result.addAllElements(ret)
             }
