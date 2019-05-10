0a76fca9bd514ac3a08b08dd256b154ef8ea5d73
sharkovadarya <sharkovadarya@gmail.com>
2019-05-10 15:44:37 +0300

    Moved SnakemakeBundle.properties to 'resources' folder.

--- src/main/kotlin/com/jetbrains/snakecharm/SnakemakeBundle.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/SnakemakeBundle.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.snakecharm
 
+import com.intellij.AbstractBundle
 import com.intellij.CommonBundle
 import com.intellij.reference.SoftReference
 import org.jetbrains.annotations.NonNls
@@ -7,23 +8,25 @@ import org.jetbrains.annotations.PropertyKey
 import java.lang.ref.Reference
 import java.util.*
 
-object SnakemakeBundle {
-    private var ourBundle: Reference<ResourceBundle>? = null
-    @NonNls
-    private const val BUNDLE = "com.jetbrains.snakecharm.SnakemakeBundle"
+class SnakemakeBundle {
+    companion object {
+        private var ourBundle: Reference<ResourceBundle>? = null
+        @NonNls
+        private const val BUNDLE = "SnakemakeBundle"
 
-    // Cached loading
-    private val bundle: ResourceBundle?
-        get() {
-            var bundle = SoftReference.dereference(ourBundle)
-            if (bundle == null) {
-                bundle = ResourceBundle.getBundle(BUNDLE)
-                ourBundle = SoftReference(bundle)
+        // Cached loading
+        private val bundle: ResourceBundle?
+            get() {
+                var bundle = SoftReference.dereference(ourBundle)
+                if (bundle == null) {
+                    bundle = ResourceBundle.getBundle(BUNDLE)
+                    ourBundle = SoftReference(bundle)
+                }
+                return bundle
             }
-            return bundle
-        }
 
-    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
-        return CommonBundle.message(bundle!!, key, *params)
+        fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
+            return CommonBundle.message(bundle!!, key, *params)
+        }
     }
 }
