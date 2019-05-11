35d77d771e2ad89a4832f0f84d699b09bbbb0bef
sharkovadarya <sharkovadarya@gmail.com>
2019-05-11 22:55:54 +0300

    Refactored snakemake visitor class (now SMKElementVisitor).

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/SnakemakeInspectionVisitor.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SnakemakeInspectionVisitor.kt
@@ -3,9 +3,13 @@ package com.jetbrains.snakecharm.inspections
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
 import com.jetbrains.python.inspections.PyInspectionVisitor
-import com.jetbrains.snakecharm.lang.psi.SnakemakeVisitor
+import com.jetbrains.python.psi.PyElementVisitor
+import com.jetbrains.snakecharm.lang.psi.SMKElementVisitor
 
 abstract class SnakemakeInspectionVisitor(
         holder: ProblemsHolder,
         session: LocalInspectionToolSession
-) : SnakemakeVisitor, PyInspectionVisitor(holder, session)
+) : SMKElementVisitor, PyInspectionVisitor(holder, session) {
+    override val pyElementVisitor: PyElementVisitor
+        get() = this
+}
--- src/main/kotlin/com/jetbrains/snakecharm/lang/validation/SnakemakeAnnotator.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/validation/SnakemakeAnnotator.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.snakecharm.lang.validation
 
+import com.jetbrains.python.psi.PyElementVisitor
 import com.jetbrains.python.validation.PyAnnotator
 import com.jetbrains.snakecharm.lang.psi.*
 
@@ -7,33 +8,8 @@ import com.jetbrains.snakecharm.lang.psi.*
  * @author Roman.Chernyatchik
  * @date 2019-01-09
  */
-abstract class SnakemakeAnnotator: PyAnnotator(), SnakemakeVisitor {
-    override fun visitSMKRule(smkRule: SMKRule) {
-        super.visitPyElement(smkRule)
-    }
-
-    override fun visitSMKRuleParameterListStatement(st: SMKRuleParameterListStatement) {
-        super.visitPyStatement(st)
-    }
-
-    override fun visitSMKRuleRunParameter(st: SMKRuleRunParameter) {
-        super.visitPyStatementList(st.statementList)
-    }
-
-    override fun visitSMKWorkflowParameterListStatement(st: SMKWorkflowParameterListStatement) {
-        super.visitPyStatement(st)
-    }
-
-    override fun visitSMKWorkflowPythonBlockParameter(st: SMKWorkflowPythonBlockParameter) {
-        super.visitPyStatementList(st.statementList)
-    }
-
-    override fun visitSMKWorkflowRulesReorderStatement(st: SMKWorkflowRulesReorderStatement) {
-        super.visitPyStatement(st)
-    }
-
-    override fun visitSMKWorkflowLocalRulesStatement(st: SMKWorkflowLocalRulesStatement) {
-        super.visitPyStatement(st)
-    }
+abstract class SnakemakeAnnotator: PyAnnotator(), SMKElementVisitor {
+    override val pyElementVisitor: PyElementVisitor
+        get() = this
 }
 
