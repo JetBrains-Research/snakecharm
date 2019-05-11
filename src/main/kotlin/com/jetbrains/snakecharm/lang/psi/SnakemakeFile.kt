f63273f821148ca697225d81799632cc7c521131
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2019-05-11 12:08:06 +0300

    SmakemakeFile.collectRules() added

--- src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SnakemakeFile.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SnakemakeFile.kt
@@ -1,15 +1,17 @@
 package com.jetbrains.snakecharm.lang.psi
 
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
 import com.jetbrains.python.psi.impl.PyFileImpl
 import com.jetbrains.snakecharm.SnakemakeFileType
 import com.jetbrains.snakecharm.lang.SnakemakeLanguageDialect
+import com.jetbrains.snakecharm.lang.validation.SnakemakeAnnotator
 
 /**
  * @author Roman.Chernyatchik
  * @date 2018-12-31
  */
-class SnakemakeFile(viewProvider: FileViewProvider): PyFileImpl(viewProvider, SnakemakeLanguageDialect) { // SnakemakeScopeOwner:ScopeOwner
+class SnakemakeFile(viewProvider: FileViewProvider) : PyFileImpl(viewProvider, SnakemakeLanguageDialect) { // SnakemakeScopeOwner:ScopeOwner
     // CythonFile, CythonScopeOwner
 
     override fun getIcon(flags: Int) = SnakemakeFileType.icon
@@ -17,4 +19,20 @@ class SnakemakeFile(viewProvider: FileViewProvider): PyFileImpl(viewProvider, Sn
     override fun toString() = "SnakemakeFile: $name"
 
     override fun getStub() = null
+
+    fun collectRules(): List<Pair<String, PsiElement>> {
+        // TODO: add tests, this is simple impl for internship task practice
+        val ruleNameAndPsi = arrayListOf<Pair<String, PsiElement>>()
+
+        // TODO[romeo]: refactor code: replace SnakemakeAnnotator with SMKElementVisitor
+        acceptChildren(object : SnakemakeAnnotator() {
+            override fun visitSMKRule(smkRule: SMKRule) {
+                val element = smkRule.getNameNode()?.psi
+                if (element != null) {
+                    ruleNameAndPsi.add(smkRule.name!! to element)
+                }
+            }
+        })
+        return ruleNameAndPsi
+    }
 }
