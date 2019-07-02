85b2144a6d7c707925dc558069d7aca0dbc59169
Nikita.Nazarov <nekit-000000@mail.ru>
2019-07-02 17:16:35 +0300

    Fixed code style and added additional checks

--- src/main/kotlin/com/jetbrains/snakecharm/lang/documentation/SmkShadowSettingsDocumentation.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/documentation/SmkShadowSettingsDocumentation.kt
@@ -6,21 +6,23 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.python.psi.PyStringLiteralExpression
+import com.jetbrains.snakecharm.lang.SnakemakeLanguageDialect
 import com.jetbrains.snakecharm.lang.psi.SMKRuleParameterListStatement
 
 
 class SmkShadowSettingsDocumentation : AbstractDocumentationProvider() {
     override fun generateDoc(
-            element: PsiElement?,
+            element: PsiElement,
             originalElement: PsiElement?): String? {
-        if (!element.isInShadowSection() || element?.parent !is PyStringLiteralExpression) {
+        if (!SnakemakeLanguageDialect.isInsideSmkFile(element) ||
+            !element.isInShadowSection() || element.parent !is PyStringLiteralExpression) {
             return null
         }
 
         return getDocumentation(element)
     }
 
-    private fun getDocumentation(element: PsiElement) : String? =
+    private fun getDocumentation(element: PsiElement): String? =
             when (element.text) {
                 "\"full\"" -> "The setting shadow: \"full\" fully shadows the entire subdirectory structure of the current workdir."
                 "\"shallow\"" -> "By setting shadow: \"shallow\", the top level files and directories are symlinked," +
@@ -37,6 +39,6 @@ class SmkShadowSettingsDocumentation : AbstractDocumentationProvider() {
     }
 }
 
-fun PsiElement?.isInShadowSection() : Boolean =
+fun PsiElement.isInShadowSection(): Boolean =
         PsiTreeUtil.getParentOfType(this, SMKRuleParameterListStatement::class.java)?.name ==
                 SMKRuleParameterListStatement.SHADOW
--- src/test/resources/com/jetbrains/snakecharm/cucumber/documentation/shadow_settings_documentation.feature
+++ src/test/resources/com/jetbrains/snakecharm/cucumber/documentation/shadow_settings_documentation.feature
@@ -11,7 +11,7 @@ Feature: Documentation for 'shadow' settings
     Then I invoke quick documentation popup
     Then Documentation text should contain <documentation>
     Examples:
-    | setting | documentation |
+    | setting | documentation                                                                                       |
     | full    |  The setting shadow: "full" fully shadows the entire subdirectory structure of the current workdir. |
     | shallow |  By setting shadow: "shallow", the top level files and directories are symlinked                    |
     | minimal |  The setting shadow: "minimal" only symlinks the inputs to the rule.                                |
