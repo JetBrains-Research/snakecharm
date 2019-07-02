9b831b36c55365b8eea9c36f5f147ece2c10d94a
Nikita.Nazarov <nekit-000000@mail.ru>
2019-07-02 18:55:13 +0300

    Tests rewritten and highlighting changed, also documentation improved

--- src/main/kotlin/com/jetbrains/snakecharm/lang/documentation/SmkShadowSettingsDocumentation.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/documentation/SmkShadowSettingsDocumentation.kt
@@ -6,6 +6,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.python.psi.PyStringLiteralExpression
+import com.jetbrains.snakecharm.SnakemakeBundle
 import com.jetbrains.snakecharm.lang.SnakemakeLanguageDialect
 import com.jetbrains.snakecharm.lang.psi.SMKRuleParameterListStatement
 
@@ -24,10 +25,9 @@ class SmkShadowSettingsDocumentation : AbstractDocumentationProvider() {
 
     private fun getDocumentation(element: PsiElement): String? =
             when (element.text) {
-                "\"full\"" -> "The setting shadow: \"full\" fully shadows the entire subdirectory structure of the current workdir."
-                "\"shallow\"" -> "By setting shadow: \"shallow\", the top level files and directories are symlinked," +
-                        "so that any relative paths in a subdirectory will be real paths in the filesystem."
-                "\"minimal\"" -> "The setting shadow: \"minimal\" only symlinks the inputs to the rule."
+                "\"full\"" -> SnakemakeBundle.message("INSP.NAME.shadow.settings.full")
+                "\"shallow\"" -> SnakemakeBundle.message("INSP.NAME.shadow.settings.shallow")
+                "\"minimal\"" -> SnakemakeBundle.message("INSP.NAME.shadow.settings.minimal")
                 else -> null
             }
 
