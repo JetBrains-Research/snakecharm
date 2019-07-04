300455a031abb08284d0e86e621c83050287dfec
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2019-07-04 20:28:20 +0300

    Remove syntax error from 'return' in run, onerror, onsuccess, onstart, see #14

---
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/highlighter/SnakemakeVisitorFilter.kt
@@ -0,0 +1,18 @@
+package com.jetbrains.snakecharm.lang.highlighter
+
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.inspections.PythonVisitorFilter
+import com.jetbrains.python.psi.PyElementVisitor
+import com.jetbrains.python.validation.ReturnAnnotator
+
+class SnakemakeVisitorFilter: PythonVisitorFilter {
+    private val unsupportedClasses = listOf(
+            /** Instead use [com.jetbrains.snakecharm.lang.validation.SmkReturnAnnotator] **/
+            ReturnAnnotator::class.java
+    )
+
+    override fun isSupported(visitorClass: Class<out PyElementVisitor>, file: PsiFile) = !unsupportedClasses
+        .any { unsupportedClass ->
+            unsupportedClass === visitorClass
+        }
+}
