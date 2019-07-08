cfd1a55e4c3235575dbf3850bdc5a33e0b9830ec
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-07-08 17:28:03 +0300

    Refactoring.

--- src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SMKRuleReference.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SMKRuleReference.kt
@@ -25,22 +25,23 @@ class SMKRuleReference(node: ASTNode): PyElementImpl(node), PsiNamedElement{
     override fun getReference(): PsiReference? = SMKRulePSIReference(this, TextRange(0, textLength))
 
     private fun getNameNode() = getIdentifierNode(node)
+
+    private class SMKRulePSIReference(
+            element: PsiElement,
+            textRange: TextRange
+    ) : PsiReferenceBase<PsiElement>(element, textRange) {
+        private val key: String = element.text
+
+        override fun resolve(): PsiElement? =
+                getRules().firstOrNull { it.first == key }?.second ?:
+                getCheckpoints().firstOrNull { it.first == key }?.second
+
+        private fun getRules() = PsiTreeUtil.getParentOfType(element, SnakemakeFile::class.java)
+                ?.collectRules() ?: emptyList()
+
+        private fun getCheckpoints() = PsiTreeUtil.getParentOfType(element, SnakemakeFile::class.java)
+                ?.collectCheckPoints()
+                ?: emptyList()
+    }
 }
 
-class SMKRulePSIReference(
-        element: PsiElement,
-        textRange: TextRange
-) : PsiReferenceBase<PsiElement>(element, textRange) {
-    private val key: String = element.text
-
-    override fun resolve(): PsiElement? =
-            getRules().firstOrNull { it.first == key }?.second ?:
-            getCheckpoints().firstOrNull { it.first == key }?.second
-
-    private fun getRules() = PsiTreeUtil.getParentOfType(element, SnakemakeFile::class.java)
-            ?.collectRules() ?: emptyList()
-
-    private fun getCheckpoints() = PsiTreeUtil.getParentOfType(element, SnakemakeFile::class.java)
-            ?.collectCheckPoints()
-            ?: emptyList()
-}
