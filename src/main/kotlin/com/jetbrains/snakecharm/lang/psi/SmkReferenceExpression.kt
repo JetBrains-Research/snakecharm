d5b0d547b16a4d77e520636a5ce70944a2b365c4
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-07-09 16:20:08 +0300

    Added multiresolve for SmkReferenceExpression + test.

--- src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SmkReferenceExpression.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SmkReferenceExpression.kt
@@ -4,13 +4,11 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiNamedElement
-import com.intellij.psi.PsiReference
-import com.intellij.psi.PsiReferenceBase
+import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.python.psi.PyUtil
 import com.jetbrains.python.psi.impl.PyElementImpl
+import com.jetbrains.python.psi.resolve.RatedResolveResult
 
 class SmkReferenceExpression(node: ASTNode): PyElementImpl(node), PsiNamedElement{
     override fun getName() = getNameNode()?.text
@@ -29,14 +27,21 @@ class SmkReferenceExpression(node: ASTNode): PyElementImpl(node), PsiNamedElemen
     private fun getNameNode() = getIdentifierNode(node)
 
     private class SmkRuleOrCheckpointNameReference(
-            element: PsiElement,
+            private val element: PsiNamedElement,
             textRange: TextRange
-    ) : PsiReferenceBase<PsiElement>(element, textRange) {
+    ) : PsiReferenceBase<PsiElement>(element, textRange), PsiPolyVariantReference {
         private val key: String = element.text
 
         override fun resolve(): PsiElement? =
                 getRules().firstOrNull { it.first == key }?.second ?:
                 getCheckpoints().firstOrNull { it.first == key }?.second
+
+        override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
+            val elements = mutableListOf<PsiElement>()
+            elements.addAll(getRules().filter { it.first == key }.map { it.second })
+            elements.addAll(getCheckpoints().filter { it.first == key }.map { it.second })
+            return elements.map { RatedResolveResult(RatedResolveResult.RATE_NORMAL, it) }.toTypedArray()
+        }
 
         override fun getVariants(): Array<Any> {
             val variants = mutableListOf<LookupElement>()
@@ -52,7 +57,7 @@ class SmkReferenceExpression(node: ASTNode): PyElementImpl(node), PsiNamedElemen
         }
 
         override fun handleElementRename(newElementName: String): PsiElement =
-                element.let { (it as PsiNamedElement).setName(newElementName) }
+                element.let { it.setName(newElementName) }
 
         private fun getRules() = PsiTreeUtil.getParentOfType(element, SnakemakeFile::class.java)
                 ?.collectRules() ?: emptyList()
--- src/test/resources/com/jetbrains/snakecharm/cucumber/resolve/localrules_and_ruleorder.feature
+++ src/test/resources/com/jetbrains/snakecharm/cucumber/resolve/localrules_and_ruleorder.feature
@@ -96,7 +96,33 @@ Feature: Resolve for rules in localrules and ruleorder
       | ptn             | text      | symbol_name | file         |
       | > bbb           | bbbb      | bbbb        | foo.smk      |
       | > ccc           | cccc      | cccc        | foo.smk      |
-      | ruleorder: aa   | aaaa      | aaaa        | foo.smk       |
+      | ruleorder: aa   | aaaa      | aaaa        | foo.smk      |
+
+  Scenario Outline: Multiresolve in ruleorder section
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    ruleorder: aaaa
+
+    rule aaaa: #1
+      input: "input.txt"
+
+    rule aaaa: #2
+      output: touch("output.txt")
+
+    checkpoint aaaa: #3
+      output: touch("_output.txt")
+    """
+    When I put the caret after <ptn>
+    Then reference should multi resolve to name, file, times
+      | aaaa (SMKRule)       | foo.smk | 2 |
+      | aaaa (SMKCheckPoint) | foo.smk | 1 |
+
+    Examples:
+      | ptn             |
+      | ruleorder: aa   |
+      | ruleorder: aa   |
+      | ruleorder: aa   |
 
 
 
