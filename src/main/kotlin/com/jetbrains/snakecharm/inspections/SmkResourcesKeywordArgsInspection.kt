c68933ba69a8e917e5a0c784461867b2c33cbea8
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-07-17 16:21:29 +0300

    Added IntentionAction for SmkResourcesKeywordArgsInspection.

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkResourcesKeywordArgsInspection.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkResourcesKeywordArgsInspection.kt
@@ -1,7 +1,10 @@
 package com.jetbrains.snakecharm.inspections
 
-import com.intellij.codeInspection.LocalInspectionToolSession
-import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInsight.FileModificationService
+import com.intellij.codeInspection.*
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
 import com.jetbrains.python.psi.PyKeywordArgument
 import com.jetbrains.snakecharm.SnakemakeBundle
 import com.jetbrains.snakecharm.lang.SnakemakeNames
@@ -20,11 +23,48 @@ class SmkResourcesKeywordArgsInspection : SnakemakeInspection() {
 
             st.argumentList?.arguments?.forEach {
                 if (it !is PyKeywordArgument) {
-                    registerProblem(it, SnakemakeBundle.message("INSP.NAME.resources.unnamed.args"))
+                    val action = MoveCaretAndInsertEqualsSignAction(SmartPointerManager.createPointer(it))
+                    registerProblem(
+                            it,
+                            SnakemakeBundle.message("INSP.NAME.resources.unnamed.args"),
+                            ProblemHighlightType.ERROR,
+                            action,
+                            action
+                    )
                 }
             }
         }
     }
 
     override fun getDisplayName(): String = SnakemakeBundle.message("INSP.NAME.resources.unnamed.args")
+
+    private class MoveCaretAndInsertEqualsSignAction(
+            private val elementPointer: SmartPsiElementPointer<PsiElement>
+    ) : HintAction, LocalQuickFixAndIntentionActionOnPsiElement(elementPointer.element) {
+        override fun startInWriteAction() = true
+
+        override fun getFamilyName(): String = SnakemakeBundle.message("INSP.INTN.name.resource.family")
+
+        override fun showHint(editor: Editor) = true
+
+        override fun getText(): String = SnakemakeBundle.message("INSP.INTN.name.resource.text")
+
+        override fun invoke(
+                project: Project,
+                file: PsiFile,
+                editor: Editor?,
+                startElement: PsiElement,
+                endElement: PsiElement
+        ) {
+            if (!FileModificationService.getInstance().prepareFileForWrite(file)) {
+                return
+            }
+
+            val offset = elementPointer.element?.textOffset ?: return
+            editor?.caretModel?.moveToOffset(offset)
+            if (editor?.document?.isWritable == true) {
+                editor.document.insertString(offset, "=")
+            }
+        }
+    }
 }
