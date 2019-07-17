16372e9ccb7b130aced1ee02aa855ac8e0dd91e6
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-07-17 18:16:26 +0300

    Added HintAction for SmkRuleSectionAfterExecutionInspection.

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkRuleSectionAfterExecutionInspection.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkRuleSectionAfterExecutionInspection.kt
@@ -1,7 +1,15 @@
 package com.jetbrains.snakecharm.inspections
 
+import com.intellij.codeInspection.HintAction
 import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.problems.Problem
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.psi.PyUtil
 import com.jetbrains.snakecharm.SnakemakeBundle
 import com.jetbrains.snakecharm.lang.psi.*
 
@@ -20,8 +28,7 @@ class SmkRuleSectionAfterExecutionInspection : SnakemakeInspection() {
         }
 
         private fun visitSMKRuleLike(rule: SmkRuleLike<SmkArgsSection>) {
-            var executionSectionOccurred = false
-            var executionSectionName: String? = null
+            var executionSection: SmkRuleOrCheckpointArgsSection? = null
 
             val sections = rule.getSections()
             for (st in sections) {
@@ -29,24 +36,65 @@ class SmkRuleSectionAfterExecutionInspection : SnakemakeInspection() {
                     val sectionName = st.sectionKeyword ?: return
                     val isExecutionSection = sectionName in SmkRuleOrCheckpointArgsSection.EXECUTION_KEYWORDS
                     if (isExecutionSection) {
-                        executionSectionOccurred = true
-                        executionSectionName = sectionName
+                        executionSection = st
                     }
 
-                    if (executionSectionOccurred && !isExecutionSection) {
-                        requireNotNull(executionSectionName)
+                    if (executionSection != null && !isExecutionSection) {
+                        requireNotNull(executionSection.name)
 
                         registerProblem(st,
                                 SnakemakeBundle.message(
                                         "INSP.NAME.rule.section.after.execution.message",
                                         sectionName,
-                                        executionSectionName
-                                )
+                                        executionSection.name!!
+                                ),
+                                ProblemHighlightType.ERROR,
+                                SwapTwoSectionsHintAction(executionSection, st)
                         )
                     }
                 }
             }
+        }
+    }
 
+    private class SwapTwoSectionsHintAction(
+            private val firstSection: SmkRuleOrCheckpointArgsSection,
+            private val secondSection: SmkRuleOrCheckpointArgsSection
+    ) : HintAction {
+        override fun startInWriteAction() = false
+
+        override fun getFamilyName() = SnakemakeBundle.message("INSP.INTN.move.rule.section.upwards.family")
+
+        override fun getText(): String = SnakemakeBundle.message("INSP.INTN.move.rule.section.upwards.text")
+
+        override fun showHint(editor: Editor) = true
+
+        override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?) = true
+
+        override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+            val document = editor?.document ?: return
+            val startOffset1 = firstSection.textOffset
+            val endOffset1 = firstSection.textOffset + firstSection.textLength
+            val startOffset2 = secondSection.textOffset
+            val endOffset2 = secondSection.textOffset + firstSection.textLength
+            val secondSectionIndent = startOffset2 - endOffset1 - 1
+            val text1 = firstSection.text
+            // TODO what happens to multiline sections on Windows, where string separators are different?
+            WriteCommandAction.runWriteCommandAction(project) {
+                document.replaceString(startOffset1, endOffset1, secondSection.text)
+                if (text1.length > secondSection.textLength) {
+                    val endOffset = if (endOffset1 < document.textLength) endOffset1 else document.textLength
+                    document.replaceString(startOffset1 + secondSection.textLength, endOffset, "")
+                }
+                if (startOffset2 < document.textLength) {
+                    document.replaceString(startOffset2, endOffset2, "")
+                    document.insertString(startOffset2, text1)
+                } else {
+                    document.insertString(document.textLength, "\n")
+                    document.insertString(document.textLength, " ".repeat(secondSectionIndent))
+                    document.insertString(document.textLength, text1)
+                }
+            }
         }
     }
 }
