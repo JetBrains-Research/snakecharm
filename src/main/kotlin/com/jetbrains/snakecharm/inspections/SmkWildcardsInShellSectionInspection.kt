cd491cf76d613e540631dc31cf36439706843f51
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-07-31 10:31:38 +0300

    Updated inspection to collect toplevel variable names from current file and included files.

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkWildcardsInShellSectionInspection.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkWildcardsInShellSectionInspection.kt
@@ -7,10 +7,12 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.jetbrains.python.psi.PyFile
 import com.jetbrains.python.psi.PyStringLiteralExpression
+import com.jetbrains.python.psi.PyTargetExpression
 import com.jetbrains.snakecharm.SnakemakeBundle
 import com.jetbrains.snakecharm.lang.SnakemakeNames
 import com.jetbrains.snakecharm.lang.psi.SmkFile
 import com.jetbrains.snakecharm.lang.psi.SmkRuleOrCheckpointArgsSection
+import com.jetbrains.snakecharm.lang.psi.SmkWorkflowArgsSection
 import java.util.regex.Pattern
 
 class SmkWildcardsInShellSectionInspection : SnakemakeInspection() {
@@ -28,12 +30,25 @@ class SmkWildcardsInShellSectionInspection : SnakemakeInspection() {
                 return
             }
 
+            val availableVariables = mutableListOf<PyTargetExpression>()
+            availableVariables.addAll((st.containingFile as PyFile).topLevelAttributes)
+
+            val includeStatements = (st.containingFile as SmkFile)
+                    .findChildrenByClass(SmkWorkflowArgsSection::class.java)
+                    .filter { it.sectionKeyword == SnakemakeNames.WORKFLOW_INCLUDE_KEYWORD }
+            for (statement in includeStatements) {
+                for (reference in statement.references) {
+                    val resolvedFile = reference.resolve() as PyFile
+                    availableVariables.addAll(resolvedFile.topLevelAttributes)
+                }
+            }
+
             val shellCommand = st.argumentList?.arguments?.firstOrNull { it is PyStringLiteralExpression } ?: return
             val wildcardMatcher = wildcardPattern.matcher(shellCommand.text)
             while (wildcardMatcher.find()) {
                 val content = wildcardMatcher.group(1)
                 if (!SmkRuleOrCheckpointArgsSection.PARAMS_NAMES.any { content.startsWith(it) } &&
-                        !content.startsWith(wildcardsPrefix)) {
+                        !content.startsWith(wildcardsPrefix) && content !in availableVariables.map { it.name }) {
                     registerProblem(
                             shellCommand,
                             SnakemakeBundle.message("INSP.NAME.wildcards.in.shell.section"),
--- src/test/resources/com/jetbrains/snakecharm/cucumber/highlighting/wildcards_in_shell.feature
+++ src/test/resources/com/jetbrains/snakecharm/cucumber/highlighting/wildcards_in_shell.feature
@@ -54,3 +54,73 @@ Feature: Wildcard usage without 'wildcards.' prefix in shell section
       | rule_like  |
       | rule       |
       | checkpoint |
+
+  Scenario Outline: Toplevel variable in shell section
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    a = 1
+
+    <rule_like> rule1:
+      input: "file.txt"
+      output: "output.txt"
+      shell: "command {input} --key {a} > {output}"
+    """
+    And Wildcards in Shell Section inspection is enabled
+    Then I expect no inspection error
+    When I check highlighting errors
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: Toplevel variable included from another file
+    Given a snakemake project
+    Given a file "boo.smk" with text
+    """
+    b = 2
+    """
+    Given I open a file "foo.smk" with text
+    """
+    a = 1
+
+    include: "boo.smk"
+
+    <rule_like> rule1:
+      shell: "echo {a} {b}"
+    """
+    And Wildcards in Shell Section inspection is enabled
+    Then I expect no inspection error
+    When I check highlighting errors
+    Examples:
+      | rule_like  |
+      | rule       |
+
+  Scenario Outline: Local variable from an included file
+    Given a snakemake project
+    Given a file "boo.smk" with text
+    """
+    def foo():
+        b = 2
+    """
+    Given I open a file "foo.smk" with text
+    """
+    a = 1
+
+    include: "boo.smk"
+
+    <rule_like> rule1:
+      shell: "echo {a} {b}"
+    """
+    And Wildcards in Shell Section inspection is enabled
+    Then I expect inspection error on <b> in <{b}> with message
+    """
+    'wildcards' in 'shell' section can be used only with 'wildcards.' prefix
+    """
+    When I check highlighting errors
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+      | checkpoint |
+
