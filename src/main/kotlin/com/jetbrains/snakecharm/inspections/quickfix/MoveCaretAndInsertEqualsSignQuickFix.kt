08b93d415d0ea13ca4cba6317b145689b95a1312
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-08-14 18:08:56 +0300

    Modified '=' insertion to consider code style settings.

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/quickfix/MoveCaretAndInsertEqualsSignQuickFix.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/quickfix/MoveCaretAndInsertEqualsSignQuickFix.kt
@@ -1,10 +1,12 @@
 package com.jetbrains.snakecharm.inspections.quickfix
 
+import com.intellij.application.options.CodeStyle
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.jetbrains.python.formatter.PyCodeStyleSettings
 import com.jetbrains.snakecharm.SnakemakeBundle
 
 class MoveCaretAndInsertEqualsSignQuickFix(element: PsiElement) : LocalQuickFixAndIntentionActionOnPsiElement(element) {
@@ -19,10 +21,13 @@ class MoveCaretAndInsertEqualsSignQuickFix(element: PsiElement) : LocalQuickFixA
             startElement: PsiElement,
             endElement: PsiElement
     ) {
+        val pythonCodeStyleSettings = CodeStyle.getCustomSettings(file, PyCodeStyleSettings::class.java)
+        val insertionString = if (pythonCodeStyleSettings.SPACE_AROUND_EQ_IN_KEYWORD_ARGUMENT) " = " else "="
+
         val offset = startElement.textOffset
         editor?.caretModel?.moveToOffset(offset)
         if (editor?.document?.isWritable == true) {
-            editor.document.insertString(offset, "=")
+            editor.document.insertString(offset, insertionString)
         }
     }
 }
