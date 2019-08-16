828750cc3fb66bcb3341c8d79caf597abece17f3
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-08-16 11:13:07 +0300

    Updated inspection to handle rule redeclarations and not highlight rules if at least one reference resolves to a rule from a current/included file.

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkUnavailableRuleReferenceInspection.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkUnavailableRuleReferenceInspection.kt
@@ -3,14 +3,11 @@ package com.jetbrains.snakecharm.inspections
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiPolyVariantReference
 import com.jetbrains.snakecharm.SnakemakeBundle
 import com.jetbrains.snakecharm.codeInsight.resolve.SmkResolveUtil
-import com.jetbrains.snakecharm.lang.psi.SmkFile
-import com.jetbrains.snakecharm.lang.psi.SmkReferenceExpression
-import com.jetbrains.snakecharm.lang.psi.SmkWorkflowLocalrulesSection
-import com.jetbrains.snakecharm.lang.psi.SmkWorkflowRuleorderSection
+import com.jetbrains.snakecharm.lang.psi.*
 
-// TODO should we highlight in case of multiple rules with same name when one is present in current file
 class SmkUnavailableRuleReferenceInspection : SnakemakeInspection() {
     override fun buildVisitor(
             holder: ProblemsHolder,
@@ -29,10 +26,27 @@ class SmkUnavailableRuleReferenceInspection : SnakemakeInspection() {
 
         private fun checkUnresolvedReferences(references: List<SmkReferenceExpression>, file: SmkFile) {
             val includedFiles = SmkResolveUtil.getIncludedFiles(file)
-            val potentiallyUnresolvedReferences = references.filterNot { ref ->
-                val ruleOrCheckpoint = ref.reference?.resolve()
-                ruleOrCheckpoint?.containingFile == file || ruleOrCheckpoint?.containingFile in includedFiles
+            val potentiallyUnresolvedReferences = references.filter {
+                val rules = (it.reference as? PsiPolyVariantReference)
+                        ?.multiResolve(false)
+                        ?.mapNotNull { res -> res.element }
+                rules?.all { rule -> !(rule.containingFile == file || rule.containingFile in includedFiles) } ?: false
             }
+            val overriddenRulesOrCheckpoints = references.filter {
+                val rules = (it.reference as? PsiPolyVariantReference)
+                        ?.multiResolve(false)
+                        ?.mapNotNull { res -> res.element }
+                        ?.filter { rule -> rule.containingFile == file || rule.containingFile in includedFiles }
+                rules != null && rules.size > 1
+            }
+            overriddenRulesOrCheckpoints.forEach {
+                registerProblem(
+                        it,
+                        SnakemakeBundle.message("INSP.NAME.rule.redeclaration"),
+                        ProblemHighlightType.GENERIC_ERROR
+                )
+            }
+
             potentiallyUnresolvedReferences.forEach { registerProblem(
                     it, SnakemakeBundle.message("INSP.NAME.rule.not.included"), ProblemHighlightType.WEAK_WARNING
             ) }
