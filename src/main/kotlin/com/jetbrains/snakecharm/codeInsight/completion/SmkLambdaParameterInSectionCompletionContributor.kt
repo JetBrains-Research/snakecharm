3efbcda1df33a8bab25041648b4f362b710ca038
Darya.Sharkova <Darya.Sharkova@jetbrains.com>
2019-08-19 12:16:07 +0300

    Updated lambda parameter names completion contributor to handle other sections.

--- src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/SmkLambdaParameterInSectionCompletionContributor.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/SmkLambdaParameterInSectionCompletionContributor.kt
@@ -4,9 +4,11 @@ import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInsight.lookup.TailTypeDecorator
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
 import com.jetbrains.python.PyTokenTypes
+import com.jetbrains.python.psi.PyCallExpression
 import com.jetbrains.python.psi.PyLambdaExpression
 import com.jetbrains.python.psi.PyParameterList
 import com.jetbrains.snakecharm.inspections.SmkLambdaRuleParamsInspection
@@ -28,13 +30,18 @@ object SMKLambdaParameterInSectionCompletionProvider : CompletionProvider<Comple
             .inFile(SmkKeywordCompletionContributor.IN_SNAKEMAKE)
             .inside(PyParameterList::class.java)
             .inside(PyLambdaExpression::class.java)
+            .andNot(PlatformPatterns.psiElement().inside(PyCallExpression::class.java)) // don't target lambda invocations
             .inside(SmkRuleOrCheckpointArgsSection::class.java)!!
 
-    override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+    override fun addCompletions(
+            parameters: CompletionParameters,
+            context: ProcessingContext,
+            result: CompletionResultSet
+    ) {
         val element = parameters.position
         val section = PsiTreeUtil.getParentOfType(element, SmkRuleOrCheckpointArgsSection::class.java) ?: return
         when (section.name) {
-            SnakemakeNames.SECTION_INPUT -> {
+            SnakemakeNames.SECTION_INPUT, SnakemakeNames.SECTION_GROUP -> {
                 result.addElement(
                         TailTypeDecorator.withTail(
                                 LookupElementBuilder.create(SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER),
@@ -42,32 +49,43 @@ object SMKLambdaParameterInSectionCompletionProvider : CompletionProvider<Comple
                         )
                 )
             }
-            SnakemakeNames.SECTION_PARAMS -> {
-                val lambdaExpression = PsiTreeUtil.getParentOfType(element, PyLambdaExpression::class.java)!!
-                val presentParameters = lambdaExpression.parameterList.parameters.map { it.name }
-                if (SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER !in presentParameters) {
-                    result.addElement(
-                            PrioritizedLookupElement.withPriority(
-                                    LookupElementBuilder
-                                            .create(SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER)
-                                    , Double.MAX_VALUE // as this should always be the 1st completion list element
+            SnakemakeNames.SECTION_PARAMS ->
+                addCompletionResultsForSection(element, SmkLambdaRuleParamsInspection.ALLOWED_IN_PARAMS, result)
+            SnakemakeNames.SECTION_THREADS ->
+                addCompletionResultsForSection(element, SmkLambdaRuleParamsInspection.ALLOWED_IN_THREADS, result)
+            SnakemakeNames.SECTION_RESOURCES ->
+                addCompletionResultsForSection(element, SmkLambdaRuleParamsInspection.ALLOWED_IN_RESOURCES, result)
+        }
+    }
 
-                            )
+    private fun addCompletionResultsForSection(
+            element: PsiElement,
+            allowedVariants: Array<String>,
+            result: CompletionResultSet
+    ) {
+        val lambdaExpression = PsiTreeUtil.getParentOfType(element, PyLambdaExpression::class.java)!!
+        val presentParameters = lambdaExpression.parameterList.parameters.map { it.name }
+        if (SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER !in presentParameters) {
+            result.addElement(
+                    PrioritizedLookupElement.withPriority(
+                            LookupElementBuilder
+                                    .create(SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER)
+                            , Double.MAX_VALUE // as this should always be the 1st completion list element
+
                     )
-                }
+            )
+        }
 
-                val variants = SmkLambdaRuleParamsInspection.ALLOWED_IN_PARAMS
-                        .filterNot { it == SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER }
-                        .filterNot { it in presentParameters }
-                if (variants.size == 1) {
-                    result.addElement(TailTypeDecorator.withTail(
-                            LookupElementBuilder.create(variants.first()),
-                            ColonAndWhiteSpaceTail
-                    ))
-                } else {
-                    result.addAllElements(variants.map { LookupElementBuilder.create(it) })
-                }
-            }
+        val variants = allowedVariants
+                .filterNot { it == SmkLambdaRuleParamsInspection.WILDCARDS_LAMBDA_PARAMETER }
+                .filterNot { it in presentParameters }
+        if (variants.size == 1) {
+            result.addElement(TailTypeDecorator.withTail(
+                    LookupElementBuilder.create(variants.first()),
+                    ColonAndWhiteSpaceTail
+            ))
+        } else {
+            result.addAllElements(variants.map { LookupElementBuilder.create(it) })
         }
     }
 }
--- src/test/resources/com/jetbrains/snakecharm/cucumber/completion/lambda_parameter_names_in_sections.feature
+++ src/test/resources/com/jetbrains/snakecharm/cucumber/completion/lambda_parameter_names_in_sections.feature
@@ -1,22 +1,24 @@
 Feature: Completion for lambda parameter names in specific sections
 
-  Scenario Outline: Completion in input section
+  Scenario Outline: Completion in input and group section
     Given a snakemake project
     Given I open a file "foo.smk" with text
     """
     <rule_like> rule1:
-      input: lambda w
+      <section>: lambda w #
     """
     When I put the caret after lambda w
     Then I invoke autocompletion popup and see a text:
     """
     <rule_like> rule1:
-      input: lambda wildcards: 
+      <section>: lambda wildcards:  #
     """
     Examples:
-      | rule_like  |
-      | rule       |
-      | checkpoint |
+      | section | rule_like  |
+      | input   | rule       |
+      | input   | checkpoint |
+      | group   | rule       |
+      | group   | checkpoint |
 
   Scenario Outline: Completion in params section
     Given a snakemake project
@@ -56,7 +58,7 @@ Feature: Completion for lambda parameter names in specific sections
       | rule       |
       | checkpoint |
 
-  Scenario Outline: No completion in params section with tail text for first parameters
+  Scenario Outline: No completion with tail text for first parameters in params section
     Given a snakemake project
     Given I open a file "foo.smk" with text
     """
@@ -68,6 +70,115 @@ Feature: Completion for lambda parameter names in specific sections
     """
     <rule_like> rule1:
       params: lambda wildcards,input
+    """
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: Completion in threads section
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    <rule_like> rule1:
+      threads: lambda # purely for this test because I can't insert space into a step definition
+    """
+    When I put the caret at #
+    And I invoke autocompletion popup
+    Then completion list should contain:
+      | wildcards |
+      | input     |
+      | attempt   |
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: Completion in threads section with tail text for the last parameter
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    <rule_like> rule1:
+      threads: lambda wildcards, input, #
+    """
+    When I put the caret at #
+    And I invoke autocompletion popup, select "attempt" lookup item and see a text:
+    """
+    <rule_like> rule1:
+      threads: lambda wildcards, input, attempt: #
+    """
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: No completion with tail text for first parameters in threads section
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    <rule_like> rule1:
+      threads: lambda wildcards,
+    """
+    When I put the caret after ,
+    And I invoke autocompletion popup, select "input" lookup item and see a text:
+    """
+    <rule_like> rule1:
+      threads: lambda wildcards,input
+    """
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: Completion in resources section
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    <rule_like> rule1:
+      resources: a=lambda # purely for this test because I can't insert space into a step definition
+    """
+    When I put the caret at #
+    And I invoke autocompletion popup
+    Then completion list should contain:
+      | wildcards |
+      | input     |
+      | threads   |
+      | attempt   |
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: Completion in resources section with tail text for the last parameter
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    <rule_like> rule1:
+      resources: a=lambda wildcards, input, threads, #
+    """
+    When I put the caret at #
+    And I invoke autocompletion popup, select "attempt" lookup item and see a text:
+    """
+    <rule_like> rule1:
+      resources: a=lambda wildcards, input, threads, attempt: #
+    """
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: No completion with tail text for first parameters in resources section
+    Given a snakemake project
+    Given I open a file "foo.smk" with text
+    """
+    <rule_like> rule1:
+      resources: a=lambda wildcards,
+    """
+    When I put the caret after ,
+    And I invoke autocompletion popup, select "input" lookup item and see a text:
+    """
+    <rule_like> rule1:
+      resources: a=lambda wildcards,input
     """
     Examples:
       | rule_like  |
