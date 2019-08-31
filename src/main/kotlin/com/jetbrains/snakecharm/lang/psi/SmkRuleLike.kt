b38009826540ddfe8dcbad4ae9a16adc1645df33
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2019-08-31 15:10:52 +0300

    Let's mark rule like elements as statements, because they are statements

--- src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SmkRuleLike.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/psi/SmkRuleLike.kt
@@ -1,9 +1,11 @@
 package com.jetbrains.snakecharm.lang.psi
 
 import com.intellij.psi.PsiNameIdentifierOwner
+import com.jetbrains.python.psi.PyStatement
 import com.jetbrains.python.psi.PyStatementListContainer
 
 interface SmkRuleLike<out S : SmkSection>: SmkSection, SmkToplevelSection, PyStatementListContainer,
+        PyStatement,
         //ScopeOwner,
         PsiNameIdentifierOwner {
 
