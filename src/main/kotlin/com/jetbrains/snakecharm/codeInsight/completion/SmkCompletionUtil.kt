bcc502c1037a270a50fb30e03d9e38a76cb3ee90
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2019-11-26 21:13:11 +0300

    Deprecated API cleanup:  SmkCompletionUtil.PYTHON_PRIORITY_WEIGHT (after 2019.3 release) (#125)

--- src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/SmkCompletionUtil.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/SmkCompletionUtil.kt
@@ -6,6 +6,7 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.psi.PsiElement
 import com.intellij.util.PlatformIcons
+import com.jetbrains.python.codeInsight.completion.PythonCompletionWeigher
 import javax.swing.Icon
 
 class SmkCompletionUtil {
@@ -15,12 +16,8 @@ class SmkCompletionUtil {
         const val SECTIONS_KEYS_PRIORITY = 30.0
         const val SUBSCRIPTION_INDEXES_PRIORITY = 20.0
 
-        const val PYTHON_PRIORITY_WEIGHT = 5.0
-        // < 2019.2
-        // const val PYTHON_PRIORITY_WEIGHT = PythonCompletionWeigher.WEIGHT_DELTA.toDouble()
-        // >= 2019.3
-        // const val PYTHON_PRIORITY_WEIGHT = PythonCompletionWeigher.PRIORITY_WEIGHT.toDouble()
-        
+        const val PYTHON_PRIORITY_WEIGHT = PythonCompletionWeigher.PRIORITY_WEIGHT.toDouble()
+
         fun createPrioritizedLookupElement(
                 item: LookupElement,
                 priority: Double = PYTHON_PRIORITY_WEIGHT
