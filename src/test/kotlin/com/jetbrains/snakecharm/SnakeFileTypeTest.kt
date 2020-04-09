1b68024189165d669751882de806731872f2a0e0
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-04-10 00:37:55 +0300

    cleanup: deprecated API cleanup

--- src/test/kotlin/com/jetbrains/snakecharm/SnakeFileTypeTest.kt
+++ src/test/kotlin/com/jetbrains/snakecharm/SnakeFileTypeTest.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.snakecharm
 
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.testFramework.HeavyPlatformTestCase
+import com.intellij.testFramework.LightPlatformTestCase
 import com.intellij.testFramework.PlatformTestCase
 import com.jetbrains.snakecharm.lang.psi.SmkFile
 import junit.framework.TestCase
@@ -9,7 +11,7 @@ import junit.framework.TestCase
  * @author Roman.Chernyatchik
  * @date 2019-02-02
  */
-class SnakeFileTypeTest: PlatformTestCase() {
+class SnakeFileTypeTest: HeavyPlatformTestCase() {
     fun testSnakefile() {
         doTest(prefix = "Snakefile", extension = null)
     }
@@ -25,7 +27,7 @@ class SnakeFileTypeTest: PlatformTestCase() {
     private fun doTest(extension: String?, prefix: String="test") {
         val dir = createTempDirectory()
         val file = FileUtil.createTempFile(dir, prefix, extension, true)
-        val virtualFile = PlatformTestCase.getVirtualFile(file)
+        val virtualFile = getVirtualFile(file)
         TestCase.assertNotNull(virtualFile)
         val psi = psiManager.findFile(virtualFile)
         TestCase.assertTrue(psi is SmkFile)
