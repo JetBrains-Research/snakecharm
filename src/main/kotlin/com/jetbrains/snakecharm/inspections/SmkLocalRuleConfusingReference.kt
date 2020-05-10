1976c672e923cd3281374727b0da91f2a74cafe5
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-05-10 16:21:48 +0300

    Feature: Inspection: rule from other file in localrules #287

---
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkLocalRuleConfusingReference.kt
@@ -0,0 +1,44 @@
+package com.jetbrains.snakecharm.inspections
+
+import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.jetbrains.snakecharm.SnakemakeBundle
+import com.jetbrains.snakecharm.lang.psi.SmkFile
+import com.jetbrains.snakecharm.lang.psi.SmkReferenceExpression
+import com.jetbrains.snakecharm.lang.psi.SmkWorkflowLocalrulesSection
+
+class SmkLocalRuleConfusingReference : SnakemakeInspection() {
+    override fun buildVisitor(
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean,
+        session: LocalInspectionToolSession
+    ) = object : SnakemakeInspectionVisitor(holder, session) {
+        override fun visitSmkWorkflowLocalrulesSection(st: SmkWorkflowLocalrulesSection) {
+            val file = st.containingFile
+            if (file !is SmkFile) {
+                return
+            }
+
+            val args = st.argumentList?.arguments
+                ?.filterIsInstance(SmkReferenceExpression::class.java)
+
+            if (args != null) {
+                val rules = file.collectRules().map { it.first }
+                val checkPoints = file.collectCheckPoints().map { it.first }
+                val ruleLike = rules.plus(checkPoints).toSet()
+                args.forEach { expr ->
+                    val name = expr.name
+                    if (name != null && name !in ruleLike) {
+                        registerProblem(
+                            expr,
+                            SnakemakeBundle.message("INSP.NAME.localrule.confusing.ref.msg", name),
+                            ProblemHighlightType.WEAK_WARNING
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ src/test/resources/features/highlighting/inspections/confusing_localrule_reference.feature
@@ -0,0 +1,51 @@
+Feature: Inspection warns about confusing localrules names.
+
+  Scenario Outline: Confusing ref
+    Given a snakemake project
+    And a file "boo.smk" with text
+    """
+    <rule_like> boo:
+      input: "in"
+    """
+    And I open a file "foo.smk" with text
+    """
+    localrules: foo2, boo, foo1
+    rule foo1:
+      input: "in"
+
+    checkpoint foo2:
+      input: "in"
+    """
+    When SmkLocalRuleConfusingReference inspection is enabled
+    Then I expect inspection weak warning on <boo> with message
+    """
+    Rule 'boo' isn't defined in this file, not an error but it is confusing.
+    """
+    When I check highlighting weak warnings
+    Examples:
+      | rule_like  |
+      | rule       |
+      | checkpoint |
+
+  Scenario Outline: No confusing ref when overridden
+    Given a snakemake project
+    And a file "boo.smk" with text
+    """
+    <rule_like1> boo:
+      input: "in"
+    """
+    Given I open a file "foo.smk" with text
+    """
+    localrules: boo
+    <rule_like2> boo:
+      input: "in"
+    """
+    When SmkLocalRuleConfusingReference inspection is enabled
+    Then I expect no inspection weak warning
+    When I check highlighting weak warnings
+    Examples:
+      | rule_like1 | rule_like2 |
+      | rule       | rule       |
+      | rule       | checkpoint |
+      | checkpoint | rule       |
+      | checkpoint | checkpoint |
