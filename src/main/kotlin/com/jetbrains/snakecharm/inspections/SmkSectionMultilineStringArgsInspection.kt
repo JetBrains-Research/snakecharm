5067d877bd60df0e594c48a765e15e72b31dd35e
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-07-14 19:34:59 +0300

    code refactored

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkSectionMultilineStringArgsInspection.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkSectionMultilineStringArgsInspection.kt
@@ -2,6 +2,7 @@ package com.jetbrains.snakecharm.inspections
 
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.python.psi.*
@@ -12,10 +13,15 @@ import com.jetbrains.snakecharm.lang.psi.SmkSubworkflowArgsSection
 
 class SmkSectionMultilineStringArgsInspection : SnakemakeInspection() {
     override fun buildVisitor(
-            holder: ProblemsHolder,
-            isOnTheFly: Boolean,
-            session: LocalInspectionToolSession
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean,
+        session: LocalInspectionToolSession
     ) = object : SnakemakeInspectionVisitor(holder, session) {
+        private val stringVisitor = object : MultilineStringVisitor() {
+            override fun reportSmkProblem(psiElement: PsiElement, text: String) {
+                registerProblem(psiElement, text)
+            }
+        }
 
         override fun visitSmkSubworkflowArgsSection(st: SmkSubworkflowArgsSection) {
             checkArgumentList(st.argumentList)
@@ -25,42 +31,50 @@ class SmkSectionMultilineStringArgsInspection : SnakemakeInspection() {
             checkArgumentList(st.argumentList)
         }
 
-        private val instanceArgumentVisitor: SmkMultilineStringArgsInspectionVisitor? = null
-        val myArgumentVisitor: SmkMultilineStringArgsInspectionVisitor
-            get() = instanceArgumentVisitor ?: SmkMultilineStringArgsInspectionVisitor { arg, name ->
-                registerProblem(arg,
-                        SnakemakeBundle.message("INSP.NAME.section.multiline.string.args.message",
-                                name))
-            }
-
-        private fun checkArgumentList(
-                argumentList: PyArgumentList?
-        ) {
-            val args = argumentList?.arguments ?: emptyArray()
-            args.forEach { arg ->
-                arg.accept(myArgumentVisitor)
+        private fun checkArgumentList(argumentList: PyArgumentList?) {
+            argumentList?.arguments?.forEach { arg ->
+                arg.accept(stringVisitor)
             }
         }
     }
 
     override fun getDisplayName(): String = SnakemakeBundle.message("INSP.NAME.section.multiline.string.args")
-}
 
-class SmkMultilineStringArgsInspectionVisitor(val warnAction: (PyExpression, String) -> Unit) : PyElementVisitor() {
-    override fun visitPyBinaryExpression(node: PyBinaryExpression) {
-        node.acceptChildren(this)
-    }
 
-    override fun visitPyParenthesizedExpression(node: PyParenthesizedExpression) {
-        node.acceptChildren(this)
-    }
+    abstract class MultilineStringVisitor : PyElementVisitor() {
+        abstract fun reportSmkProblem(psiElement: PsiElement, text: String)
 
-    override fun visitPyStringLiteralExpression(node: PyStringLiteralExpression) {
-        if (node.decodedFragments.size > 1 && node.stringElements.any { x ->
-                    x.nextSibling is PsiWhiteSpace && x.nextSibling.textContains('\n')
-                }) {
-            val section = PsiTreeUtil.getParentOfType(node, SmkArgsSection::class.java)
-            warnAction(node, section?.sectionKeyword!!)
+        override fun visitPyBinaryExpression(node: PyBinaryExpression) {
+            node.acceptChildren(this)
+        }
+
+        override fun visitPyParenthesizedExpression(node: PyParenthesizedExpression) {
+            node.acceptChildren(this)
+        }
+
+        override fun visitPyStringLiteralExpression(strExpr: PyStringLiteralExpression) {
+            if (strExpr.decodedFragments.size < 2) {
+                // N/A
+                return
+            }
+
+            val isMultilineString = strExpr.stringElements.any { x ->
+                x.nextSibling is PsiWhiteSpace && x.nextSibling.textContains('\n')
+            }
+            if (!isMultilineString) {
+                // N/A
+                return
+            }
+
+            val section = PsiTreeUtil.getParentOfType(strExpr, SmkArgsSection::class.java)
+            val sectionKeyword = section?.sectionKeyword
+
+            if (sectionKeyword != null) {
+                reportSmkProblem(
+                    strExpr,
+                    SnakemakeBundle.message("INSP.NAME.section.multiline.string.args.message", sectionKeyword)
+                )
+            }
         }
     }
 }
