578a6bcfa12c91c8df9c60beb54843e8257a2512
Simon Tsirikov <tsirikov.s.a@gmail.com>
2020-07-24 13:49:04 +0300

    inspection for unexpected keyword args + tests

---
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/SmkSectionUnexpectedKeywordArgsInspection.kt
@@ -0,0 +1,50 @@
+package com.jetbrains.snakecharm.inspections
+
+import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemsHolder
+import com.jetbrains.python.psi.PyArgumentList
+import com.jetbrains.python.psi.PyKeywordArgument
+import com.jetbrains.snakecharm.SnakemakeBundle
+import com.jetbrains.snakecharm.codeInsight.SnakemakeAPI.PROHIBITED_NAMED_ARGS_SECTION_KEYWORDS
+import com.jetbrains.snakecharm.lang.psi.SmkArgsSection
+import com.jetbrains.snakecharm.lang.psi.SmkRuleOrCheckpointArgsSection
+import com.jetbrains.snakecharm.lang.psi.SmkSubworkflowArgsSection
+
+class SmkSectionUnexpectedKeywordArgsInspection : SnakemakeInspection() {
+    override fun buildVisitor(
+            holder: ProblemsHolder,
+            isOnTheFly: Boolean,
+            session: LocalInspectionToolSession
+    ) = object : SnakemakeInspectionVisitor(holder, session) {
+
+        override fun visitSmkSubworkflowArgsSection(st: SmkSubworkflowArgsSection) {
+            checkArgumentList(st.argumentList, st)
+        }
+
+        override fun visitSmkRuleOrCheckpointArgsSection(st: SmkRuleOrCheckpointArgsSection) {
+            if (st.sectionKeyword in PROHIBITED_NAMED_ARGS_SECTION_KEYWORDS) {
+                checkArgumentList(st.argumentList, st)
+            }
+        }
+
+        private fun checkArgumentList(
+                argumentList: PyArgumentList?,
+                section: SmkArgsSection
+        ) {
+            val args = argumentList?.arguments ?: emptyArray()
+            args.forEach { arg ->
+                if (arg is PyKeywordArgument) {
+                    registerProblem(
+                            arg,
+                            SnakemakeBundle.message(
+                                    "INSP.NAME.section.unexpected.keyword.args.message",
+                                    section.sectionKeyword!!
+                            )
+                    )
+                }
+            }
+        }
+    }
+
+    override fun getDisplayName(): String = SnakemakeBundle.message("INSP.NAME.section.unexpected.keyword.args")
+}
