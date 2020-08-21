236609070e88a098148928145639f1877d58e885
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-08-21 18:47:14 +0300

    code simplified

--- src/main/kotlin/com/jetbrains/snakecharm/lang/SmkWildcardConstraintsRegExpInjector.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/SmkWildcardConstraintsRegExpInjector.kt
@@ -6,11 +6,10 @@ import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.python.codeInsight.PyInjectionUtil
 import com.jetbrains.python.codeInsight.PyInjectorBase
 import com.jetbrains.python.codeInsight.regexp.PythonRegexpLanguage
+import com.jetbrains.python.psi.PyKeywordArgument
 import com.jetbrains.python.psi.PyStringLiteralExpression
-import com.jetbrains.python.psi.impl.PyKeywordArgumentImpl
 import com.jetbrains.snakecharm.lang.SnakemakeNames.WORKFLOW_WILDCARD_CONSTRAINTS_KEYWORD
-import com.jetbrains.snakecharm.lang.psi.SmkRuleOrCheckpointArgsSection
-import com.jetbrains.snakecharm.lang.psi.SmkWorkflowArgsSection
+import com.jetbrains.snakecharm.lang.psi.SmkArgsSection
 
 open class SmkWildcardConstraintsRegExpInjector : PyInjectorBase() {
     override fun getInjectedLanguage(element: PsiElement) = PythonRegexpLanguage.INSTANCE!!
@@ -26,18 +25,16 @@ open class SmkWildcardConstraintsRegExpInjector : PyInjectorBase() {
     }
 
     private fun PsiElement.isInPyKeywordArgsSection(): Boolean {
-        val parentPyKeywordArgumentImpl = PsiTreeUtil.getParentOfType(this, PyKeywordArgumentImpl::class.java)
+        val parentPyKeywordArgumentImpl = PsiTreeUtil.getParentOfType(this, PyKeywordArgument::class.java)
 
         return (parentPyKeywordArgumentImpl != null && parentPyKeywordArgumentImpl.isInWildcardConstraintsSection())
     }
 
     private fun PsiElement.isInWildcardConstraintsSection(): Boolean {
-        val parentSmkRuleOrCheckpointArgsSection = PsiTreeUtil.getParentOfType(this, SmkRuleOrCheckpointArgsSection::class.java)
+        val keyword = PsiTreeUtil.getParentOfType(this, SmkArgsSection::class.java)?.sectionKeyword
 
-        val parentSmkWorkflowArgsSection = PsiTreeUtil.getParentOfType(this, SmkWorkflowArgsSection::class.java)
-
-        return parentSmkRuleOrCheckpointArgsSection?.name == WORKFLOW_WILDCARD_CONSTRAINTS_KEYWORD ||
-                parentSmkWorkflowArgsSection?.firstChild?.text == WORKFLOW_WILDCARD_CONSTRAINTS_KEYWORD
+        return keyword == WORKFLOW_WILDCARD_CONSTRAINTS_KEYWORD ||
+                keyword == WORKFLOW_WILDCARD_CONSTRAINTS_KEYWORD
     }
 
     private fun PsiElement.isValidForInjection(): Boolean =
