897efd171fab87b7c75a1918477a655db6c59e75
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-12-18 11:27:31 +0300

    Chore: missed file

--- src/main/kotlin/com/jetbrains/snakecharm/facet/SmkFacetState.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.snakecharm.facet
-
-data class SmkFacetState(
-    val useBundledWrappersInfo: Boolean = true,
-    val wrappersCustomSourcesFolder: String = "" // use system independent separators
-)
