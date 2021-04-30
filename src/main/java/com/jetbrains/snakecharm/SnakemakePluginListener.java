794b158c50316a95387776b7cc92532359f149b3
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2021-04-30 11:05:05 +0300

    release notes updated

--- src/main/java/com/jetbrains/snakecharm/SnakemakePluginListener.java
+++ src/main/java/com/jetbrains/snakecharm/SnakemakePluginListener.java
@@ -2,5 +2,8 @@ package com.jetbrains.snakecharm;
 
 import com.intellij.ide.plugins.DynamicPluginListener;
 
+/**
+ * Read about dynamic plugins: https://plugins.jetbrains.com/docs/intellij/dynamic-plugins.html#code
+ */
 public class SnakemakePluginListener implements DynamicPluginListener {
 }
