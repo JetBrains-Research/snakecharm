0521695e3f7683ae91948e7b225c147b8f47fcc4
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2022-01-15 16:42:16 +0300

    refactor: one long method split in several short

--- src/main/kotlin/com/jetbrains/snakecharm/inspections/quickfix/CreateMissedFile.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/inspections/quickfix/CreateMissedFile.kt
@@ -2,7 +2,10 @@ package com.jetbrains.snakecharm.inspections.quickfix
 
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement
 import com.intellij.notification.NotificationType
-import com.intellij.openapi.command.undo.*
+import com.intellij.openapi.command.undo.DocumentReference
+import com.intellij.openapi.command.undo.DocumentReferenceManager
+import com.intellij.openapi.command.undo.UndoManager
+import com.intellij.openapi.command.undo.UndoableAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.progress.ProgressIndicator
@@ -10,6 +13,7 @@ import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -20,10 +24,9 @@ import org.apache.commons.io.FileUtils
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.InvalidPathException
+import java.nio.file.Path
 import java.nio.file.Paths
-import kotlin.io.path.Path
-import kotlin.io.path.isDirectory
-import kotlin.io.path.notExists
+import kotlin.io.path.*
 
 class CreateMissedFile(
     element: PsiElement,
@@ -63,15 +66,18 @@ class CreateMissedFile(
         startElement: PsiElement,
         endElement: PsiElement
     ) {
-        val relativeDirectory =
-            if (searchRelativelyToCurrentFolder) file.virtualFile.parent else ProjectRootManager.getInstance(project).fileIndex.getContentRootForFile(
-                file.virtualFile
-            ) ?: return
-        val targetFilePath = try {
-            if (Path(fileName).isAbsolute) {
-                Path(fileName)
-            } else {
-                Paths.get(relativeDirectory.path, fileName)
+        val vFile = file.virtualFile ?: return
+
+        val fileIndex = ProjectRootManager.getInstance(project).fileIndex
+        val relativeDirectory = when {
+            searchRelativelyToCurrentFolder -> vFile.parent
+            else -> fileIndex.getContentRootForFile(vFile)
+        } ?: return
+
+        val fileToCreate = try {
+            when {
+                Path(fileName).isAbsolute -> Path(fileName)
+                else -> Paths.get(relativeDirectory.path, fileName)
             }
         } catch (e: InvalidPathException) {
             LOGGER.error(e)
@@ -83,97 +89,116 @@ class CreateMissedFile(
             )
             return
         }
-        var firstAffectedFile = targetFilePath
-        while (firstAffectedFile.parent.notExists()) {
-            firstAffectedFile = firstAffectedFile.parent ?: break
-        }
-
-        val undo = Runnable {
-            if (firstAffectedFile == null || firstAffectedFile.notExists()) {
-                return@Runnable
-            }
-            if (firstAffectedFile.isDirectory()) {
-                FileUtils.deleteDirectory(firstAffectedFile.toFile())
-            } else {
-                Files.delete(firstAffectedFile)
-            }
-        }
-        val redo = Runnable {
-            try {
-                Files.createDirectories(targetFilePath.parent)
-                Files.createFile(targetFilePath)
-                val context = supportedSections[sectionName]
-                if (context != null) {
-                    // We don't use the result of 'createChildFile()' because it has inappropriate type (and throws UnsupportedOperationException)
-                    targetFilePath.toFile().appendText(context)
-                }
-            } catch (e: SecurityException) {
-                LOGGER.error(e)
-                val message = e.message ?: return@Runnable
-                SmkNotifier.notify(
-                    title = SnakemakeBundle.message("notifier.msg.create.env.file.title"),
-                    content = message,
-                    type = NotificationType.ERROR,
-                    project = project
-                )
-            } catch (e: IOException) {
-                LOGGER.error(e)
-                SmkNotifier.notify(
-                    title = SnakemakeBundle.message("notifier.msg.create.env.file.title"),
-                    content = SnakemakeBundle.message("notifier.msg.create.env.file.io.exception", fileName),
-                    type = NotificationType.ERROR,
-                    project = project
-                )
-            }
-        }
-        val action = object : UndoableAction {
-            override fun undo() {
-                // invoke in such way because:
-                // (1) changing document inside undo/redo is not allowed (see ChangeFileEncodingAction)
-                // (2) we don't want to use UI thread
-                ProgressManager.getInstance().run(object : Task.Backgroundable(
-                    project,
-                    SnakemakeBundle.message("notifier.msg.deleting.env.file", fileName),
-                    false
-                ) {
-                    override fun run(indicator: ProgressIndicator) {
-                        undo.run()
-                    }
-
-                    override fun onSuccess() {
-                        VirtualFileManager.getInstance().syncRefresh()
-                    }
-                })
-            }
-
-            override fun redo() {
-                // invoke in such way because:
-                // (1) changing document inside undo/redo is not allowed (see ChangeFileEncodingAction)
-                // (2) we don't want to use UI thread
-                ProgressManager.getInstance().run(object : Task.Backgroundable(
-                    project,
-                    SnakemakeBundle.message("notifier.msg.creating.env.file", fileName),
-                    false
-                ) {
-                    override fun run(indicator: ProgressIndicator) {
-                        redo.run()
-                    }
-
-                    override fun onSuccess() {
-                        VirtualFileManager.getInstance().syncRefresh()
-                    }
-                })
-            }
-
-            override fun getAffectedDocuments(): Array<DocumentReference> =
-            // We mark only the current file as affected to make feature convenient.
-            // Otherwise, new file opening will be regarded as a new action
-                // and 'undone' will be banned
-                arrayOf(DocumentReferenceManager.getInstance().create(file.virtualFile))
-
-            override fun isGlobal() = true
-        }
+        val action = createdUndoableBackgroundAction(
+            project, vFile, undoLambda(fileToCreate), redoLambda(fileToCreate, project)
+        )
         action.redo()
         UndoManager.getInstance(project).undoableActionPerformed(action)
+    }
+
+    private fun createdUndoableBackgroundAction(
+        project: Project,
+        actionInvocationTarget: VirtualFile,
+        undoStep: () -> Unit,
+        redoStep: () -> Unit
+    ) = object : UndoableAction {
+        override fun undo() {
+            // invoke in such way because:
+            // (1) changing document inside undo/redo is not allowed (see ChangeFileEncodingAction)
+            // (2) we don't want to use UI thread
+            ProgressManager.getInstance().run(object : Task.Backgroundable(
+                project,
+                SnakemakeBundle.message("notifier.msg.deleting.env.file", fileName),
+                false
+            ) {
+                override fun run(indicator: ProgressIndicator) = undoStep()
+
+                override fun onSuccess() {
+                    VirtualFileManager.getInstance().syncRefresh()
+                }
+            })
+        }
+
+        override fun redo() {
+            // invoke in such way because:
+            // (1) changing document inside undo/redo is not allowed (see ChangeFileEncodingAction)
+            // (2) we don't want to use UI thread
+            ProgressManager.getInstance().run(object : Task.Backgroundable(
+                project,
+                SnakemakeBundle.message("notifier.msg.creating.env.file", fileName),
+                false
+            ) {
+                override fun run(indicator: ProgressIndicator) = redoStep()
+
+                override fun onSuccess() {
+                    VirtualFileManager.getInstance().syncRefresh()
+                    // TODO: write action is required!
+                }
+            })
+        }
+
+        override fun getAffectedDocuments(): Array<DocumentReference> {
+            // * If not affected files - action not in undo/redo
+            // * If 'all' affected files - action undo doesn't work
+
+            // So:mark only the current editor file as affected to make feature convenient.
+            // Otherwise, new file opening will be regarded as a new action
+            // and 'undone' will be banned
+            return arrayOf(DocumentReferenceManager.getInstance().create(actionInvocationTarget))
+        }
+
+        override fun isGlobal() = true
+    }
+
+    private fun redoLambda(
+        fileToCreate: Path,
+        project: Project,
+    ): () -> Unit = {
+        try {
+            Files.createDirectories(fileToCreate.parent)
+            val targetFile = Files.createFile(fileToCreate)
+            val context = supportedSections[sectionName]
+            if (context != null) {
+                // We don't use the result of 'createChildFile()' because it has inappropriate
+                // type (and throws UnsupportedOperationException)
+                targetFile.appendText(context)
+            }
+        } catch (e: SecurityException) {
+            LOGGER.error(e)
+            val message = e.message ?: "Error: ${e.javaClass.name}"
+            SmkNotifier.notify(
+                title = SnakemakeBundle.message("notifier.msg.create.env.file.title"),
+                content = message,
+                type = NotificationType.ERROR,
+                project = project
+            )
+        } catch (e: IOException) {
+            LOGGER.error(e)
+            SmkNotifier.notify(
+                title = SnakemakeBundle.message("notifier.msg.create.env.file.title"),
+                content = SnakemakeBundle.message("notifier.msg.create.env.file.io.exception", fileName),
+                type = NotificationType.ERROR,
+                project = project
+            )
+        }
+    }
+
+    private fun undoLambda(fileToCreate: Path): () -> Unit {
+        // Memorize first directory that will be created & delete it on "undo" step
+        var firstCreatedFileOrDir = fileToCreate
+        while (firstCreatedFileOrDir.parent.notExists()) {
+            firstCreatedFileOrDir = firstCreatedFileOrDir.parent ?: break
+        }
+
+        val undo = {
+            if (firstCreatedFileOrDir.exists()) {
+                if (firstCreatedFileOrDir.isDirectory()) {
+                    FileUtils.deleteDirectory(firstCreatedFileOrDir.toFile())
+                } else {
+                    Files.delete(firstCreatedFileOrDir)
+                }
+            }
+        }
+        return undo
     }
 }
