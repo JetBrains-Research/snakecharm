2fff3a375cc7d65b5d89eba609dc9d75faf28693
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2022-04-28 18:38:49 +0300

    fix: intellij gradle plugin downgraded to 1.5.2
    See workaround for https://github.com/JetBrains/gradle-intellij-plugin/issues/960

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
     // This plugin allows you to build plugins for IntelliJ platform using specific
     // IntelliJ SDK and bundled plugins.
-    id("org.jetbrains.intellij") version "1.5.3"
+    id("org.jetbrains.intellij") version "1.5.2"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.3.1"
 }
