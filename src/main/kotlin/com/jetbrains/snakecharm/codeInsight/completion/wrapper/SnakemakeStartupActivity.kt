0d3227406bee3b383ba7e0ea0e8ed653c855a38f
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2023-04-07 22:15:24 +0200

    refactor: deprecated API usage cleanup

--- src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/wrapper/SnakemakeStartupActivity.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/codeInsight/completion/wrapper/SnakemakeStartupActivity.kt
@@ -2,14 +2,14 @@ package com.jetbrains.snakecharm.codeInsight.completion.wrapper
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.snakecharm.codeInsight.ImplicitPySymbolsProvider
 import com.jetbrains.snakecharm.framework.SmkSupportProjectSettings
 import kotlinx.serialization.ExperimentalSerializationApi
 
 @ExperimentalSerializationApi
-class SnakemakeStartupActivity : StartupActivity.Background {
-    override fun runActivity(project: Project) {
+class SnakemakeStartupActivity : ProjectActivity {
+    override suspend fun execute(project: Project) {
         val smkSettings = project.service<SmkSupportProjectSettings>()
         smkSettings.initOnStartup()
         val smkWrapperStorage = project.service<SmkWrapperStorage>()
