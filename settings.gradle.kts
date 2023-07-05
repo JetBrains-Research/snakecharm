800b2e6010c458a8b8fb96d1f3fe075a3b42ae1d
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2023-07-05 18:13:09 +0200

    chore: note how to get access to unpublished intellij gradle pluign version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+//TODO: Uncomment to allow intellij gradle plugin unpublished versions:
+//pluginManagement {
+//    repositories {
+//        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+//        gradlePluginPortal()
+//    }
+//}
 rootProject.name = "snakecharm"
