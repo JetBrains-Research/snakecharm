8296f002cdf61cc0ef0c77dee6f70614eef044a2
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2024-10-16 10:51:12 +0200

    feat:cleanup (related #536)

--- src/main/kotlin/com/jetbrains/snakecharm/lang/SnakemakeNames.kt
+++ src/main/kotlin/com/jetbrains/snakecharm/lang/SnakemakeNames.kt
@@ -47,32 +47,20 @@ object SnakemakeNames {
     const val SECTION_OUTPUT = "output"
     const val SECTION_LOG = "log"
     const val SECTION_BENCHMARK = "benchmark"
-    const val SECTION_VERSION = "version"
-    const val SECTION_MESSAGE = "message"
     const val SECTION_THREADS = "threads"
-    const val SECTION_SINGULARITY = WORKFLOW_SINGULARITY_KEYWORD
-    const val SECTION_PRIORITY = "priority"
     const val SECTION_WILDCARD_CONSTRAINTS = WORKFLOW_WILDCARD_CONSTRAINTS_KEYWORD
     const val SECTION_GROUP = "group"
     const val SECTION_CONDA = "conda" // >= 4.8
     const val SECTION_RESOURCES = "resources"
-    const val SECTION_PARAMS = "params"
     const val SECTION_SHELL = "shell"
     const val SECTION_SCRIPT = "script"
     const val SECTION_WRAPPER = "wrapper"
     const val SECTION_CWL = "cwl"
     const val SECTION_SHADOW = "shadow"
     const val SECTION_RUN = "run"
-    const val SECTION_CACHE = "cache" // >= 5.12.0
-    const val SECTION_CONTAINER = WORKFLOW_CONTAINER_KEYWORD
-    const val SECTION_CONTAINERIZED = WORKFLOW_CONTAINERIZED_KEYWORD
     const val SECTION_NOTEBOOK = "notebook"
     const val SECTION_ENVMODULES = "envmodules" // >= 5.9
-    const val SECTION_NAME = "name" // >= 5.31
-    const val SECTION_HANDOVER = "handover" // >= 6.2
-    const val SECTION_DEFAULT_TARGET = "default_target" // >= 6.15.0
     const val SECTION_TEMPLATE_ENGINE = "template_engine" // >= 7.0.0
-    const val SECTION_RETRIES = "retries" // >= 7.7.0
 
     const val RUN_SECTION_VARIABLE_RULE = "rule"
     const val RUN_SECTION_VARIABLE_JOBID = "jobid"
